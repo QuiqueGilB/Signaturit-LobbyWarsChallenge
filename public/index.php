5899f9a321912426b612deaff96835c9c982b2c2
Enrique Gil <user@enriquegil.me>
2022-10-17 20:34:44 +0200

    create bootstrap.php

--- public/index.php
+++ public/index.php
@@ -2,6 +2,6 @@
 
 use Signaturit\LobbyWarsChallenge\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/src/SharedContext/SymfonyModule/Infrastructure/Boot/bootstrap.php';
 
-return static fn(array $context) => new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+return static fn(array $context) => new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
---
+++ src/SharedContext/SymfonyModule/Infrastructure/Boot/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+use Symfony\Component\Dotenv\Dotenv;
+
+$projectDir = dirname(__DIR__ . '/../../../../../..');
+echo $projectDir ." \n";
+if (!is_file($projectDir . '/vendor/autoload_runtime.php')) {
+    throw new LogicException('Symfony Runtime is missing. Try running "composer require symfony/runtime".');
+}
+
+require_once $projectDir . '/vendor/autoload_runtime.php';
+(new Dotenv())->bootEnv($projectDir . '/.env');
