555bd41a29c97262f2a2db2bd2f7b3322ab9f85e
Enrique Gil <user@enriquegil.me>
2022-10-19 23:14:03 +0200

    resolve scores

---
+++ src/ContractContext/ContractModule/Domain/Service/ResolveSignatureScoresService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Domain\Service;
+
+use Signaturit\LobbyWarsChallenge\ContractContext\SharedModule\Domain\ValueObject\Signature;
+
+class ResolveSignatureScoresService
+{
+    public function acumulate(Signature ...$signatures): int
+    {
+        $hasKing = in_array(Signature::King, $signatures);
+
+        return array_sum(array_map(
+            static fn(Signature $signature) => $hasKing && Signature::Validator === $signature
+                ? 0
+                : $signature->score(),
+            $signatures
+        ));
+    }
+}
