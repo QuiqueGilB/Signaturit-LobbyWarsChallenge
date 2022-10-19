81ca759cfaffa413785ac031e46f53ae77008eaa
Enrique Gil <user@enriquegil.me>
2022-10-19 22:38:14 +0200

    signature enum

---
+++ src/ContractContext/SharedModule/Domain/ValueObject/Signature.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Signaturit\LobbyWarsChallenge\ContractContext\SharedModule\Domain\ValueObject;
+
+enum Signature: string
+{
+    case King = 'K';
+    case Notary = 'N';
+    case Validator = 'V';
+    case Unknown = '#';
+
+    public function score(): float
+    {
+        return match ($this) {
+            self::King => 5,
+            self::Notary => 2,
+            self::Validator => 1,
+            self::Unknown => 0
+        };
+    }
+}
