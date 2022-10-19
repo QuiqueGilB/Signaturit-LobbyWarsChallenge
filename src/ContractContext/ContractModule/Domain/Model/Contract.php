a1002c641ad4331a5dac92d99cc608c915866e37
Enrique Gil <user@enriquegil.me>
2022-10-19 23:13:52 +0200

    contract winner

--- src/ContractContext/ContractModule/Domain/Model/Contract.php
+++ src/ContractContext/ContractModule/Domain/Model/Contract.php
@@ -2,6 +2,7 @@
 
 namespace Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Domain\Model;
 
+use DateTimeImmutable;
 use Signaturit\LobbyWarsChallenge\SharedContext\SharedModule\Domain\Model\AggregateRoot;
 use Signaturit\LobbyWarsChallenge\SharedContext\SharedModule\Domain\ValueObject\Uuid;
 
@@ -9,10 +10,22 @@ class Contract extends AggregateRoot
 {
     /** @var Participant[] */
     public readonly array $participants;
+    public readonly ?Participant $winner;
 
     public function __construct(Uuid $id, Participant ...$participants)
     {
         parent::__construct($id);
         $this->participants = $participants;
+    }
+
+    public function update(Participant $winner): void
+    {
+        $this->doUpdate($winner);
+    }
+
+    private function doUpdate(Participant $winner): void
+    {
+        $this->winner = $winner;
+        $this->updatedAt = new DateTimeImmutable();
     }
 }
