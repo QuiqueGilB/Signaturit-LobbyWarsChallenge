226c10fbe708b87e1138e0d1b2841910cef249de
Enrique Gil <user@enriquegil.me>
2022-10-19 23:12:22 +0200

    fix

--- src/SharedContext/SharedModule/Domain/Model/Aggregate.php
+++ src/SharedContext/SharedModule/Domain/Model/Aggregate.php
@@ -3,15 +3,31 @@
 namespace Signaturit\LobbyWarsChallenge\SharedContext\SharedModule\Domain\Model;
 
 use DateTimeImmutable;
+use Signaturit\LobbyWarsChallenge\SharedContext\SharedModule\Domain\ValueObject\Uuid;
 
 abstract class Aggregate
 {
-    public readonly DateTimeImmutable $createdAt;
-    public readonly DateTimeImmutable $updatedAt;
+    protected readonly DateTimeImmutable $createdAt;
+    protected DateTimeImmutable $updatedAt;
 
-    public function __construct()
+    public function __construct(protected readonly Uuid $id)
     {
         $this->createdAt = new DateTimeImmutable();
         $this->updatedAt = new DateTimeImmutable();
+    }
+
+    public function id(): Uuid
+    {
+        return $this->id;
+    }
+
+    public function createdAt(): DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function updatedAt(): DateTimeImmutable
+    {
+        return $this->updatedAt;
     }
 }
