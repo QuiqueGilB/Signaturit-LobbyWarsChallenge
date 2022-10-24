4868d031fe587acc5662bbff7a7cb83a4410dfbc
Enrique Gil <user@enriquegil.me>
2022-10-24 18:50:37 +0200

    need to win service

---
+++ src/ContractContext/ContractModule/Domain/Service/ResolveNeedSignatureToWinService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Domain\Service;
+
+use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Domain\Exception\ImpossibleWinException;
+use Signaturit\LobbyWarsChallenge\ContractContext\SharedModule\Domain\ValueObject\Signature;
+
+class ResolveNeedSignatureToWinService
+{
+    public function __construct(private readonly ResolveSignatureScoresService $resolveSignatureScoresService)
+    {
+    }
+
+    /**
+     * TODO: Calculate many signatures
+     * @return Signature[]
+     */
+    public function needToWin(int $maxScore, Signature ...$signatures): array
+    {
+        $signaturesSorted = Signature::cases();
+        usort($signaturesSorted, static fn(Signature $a, Signature $b) => $a->score() <=> $b->score());
+
+        foreach ($signaturesSorted as $signature) {
+            if ($maxScore < $this->resolveSignatureScoresService->acumulate($signature, ...$signatures)) {
+                return [$signature];
+            }
+        }
+
+        throw ImpossibleWinException::onlyOnceSignature($maxScore, ...$signatures);
+    }
+}
