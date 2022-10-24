3cef1a62f0c89b00cd868c36ec8c61e8378e740c
Enrique Gil <user@enriquegil.me>
2022-10-24 19:48:16 +0200

    controller contract

--- src/SharedContext/SymfonyModule/Infrastructure/Http/Controller.php
+++ src/SharedContext/SymfonyModule/Infrastructure/Http/Controller.php
@@ -5,10 +5,14 @@ namespace Signaturit\LobbyWarsChallenge\SharedContext\SymfonyModule\Infrastructu
 use Signaturit\LobbyWarsChallenge\SharedContext\CqrsModule\Domain\Contract\CommandBus;
 use Signaturit\LobbyWarsChallenge\SharedContext\CqrsModule\Domain\Contract\QueryBus;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 abstract class Controller extends AbstractController
 {
     public function __construct(protected readonly CommandBus $commandBus, protected readonly QueryBus $queryBus)
     {
     }
+
+    abstract public function __invoke(Request $request): Response;
 }
