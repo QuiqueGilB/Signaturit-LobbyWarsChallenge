b7c09a62aa16cd437297285f303111b64f2f98e6
Enrique Gil <user@enriquegil.me>
2022-10-24 19:51:47 +0200

    fix

--- src/ContractContext/ContractModule/Infrastructure/Entrypoint/Http/PostContractController.php
+++ src/ContractContext/ContractModule/Infrastructure/Entrypoint/Http/PostContractController.php
@@ -5,23 +5,26 @@ namespace Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Infrastru
 use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Application\Command\CreateContract\CreateContractCommand;
 use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Application\Query\FindContract\FindContractQuery;
 use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Application\Query\FindContract\FindContractQueryResponse;
-use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Domain\Model\Participant;
+use Signaturit\LobbyWarsChallenge\ContractContext\SharedModule\Domain\Data\ParticipantData;
+use Signaturit\LobbyWarsChallenge\ContractContext\SharedModule\Domain\ValueObject\Signature;
 use Signaturit\LobbyWarsChallenge\SharedContext\SharedModule\Domain\ValueObject\Uuid;
 use Signaturit\LobbyWarsChallenge\SharedContext\SymfonyModule\Infrastructure\Http\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostContractController extends Controller
 {
     #[Route('jeje', methods: ['GET', 'POST'])]
-    public function __invoke()
+    public function __invoke(Request $request): Response
     {
         $contractId = Uuid::v4();
 
         $command = new CreateContractCommand(
             $contractId,
-            new Participant(Uuid::v4(), 'KV'),
-            new Participant(Uuid::v4(), 'KV')
+            new ParticipantData(Uuid::v4(), [Signature::King]),
+            new ParticipantData(Uuid::v4(), [Signature::Notary])
         );
         $this->commandBus->handle($command);
 
