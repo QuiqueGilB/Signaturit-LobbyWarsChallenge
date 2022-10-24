267899427da97b85d3d09141d6202ddda68e9888
Enrique Gil <user@enriquegil.me>
2022-10-24 18:50:45 +0200

    refactor service

--- src/ContractContext/ContractModule/Application/Command/ResolveSignatureForWin/ResolveSignatureForWinCommandHandler.php
+++ src/ContractContext/ContractModule/Application/Command/ResolveSignatureForWin/ResolveSignatureForWinCommandHandler.php
@@ -3,33 +3,34 @@
 namespace Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Application\Command\ResolveSignatureForWin;
 
 use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Domain\Contract\ContractRepository;
+use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Domain\Exception\ContractHasNotWinnerException;
 use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Domain\Exception\ContractNotFoundException;
-use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Domain\Exception\ContractWinnerException;
 use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Domain\Model\Participant;
-use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Domain\Service\ResolveSignatureScoresService;
+use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Domain\Service\ResolveNeedSignatureToWinService;
 use Signaturit\LobbyWarsChallenge\SharedContext\CqrsModule\Domain\Contract\CommandHandler;
 
 class ResolveSignatureForWinCommandHandler implements CommandHandler
 {
     public function __construct(
         private readonly ContractRepository $contractRepository,
-        private readonly ResolveSignatureScoresService $resolveSignatureScoresService
+        private readonly ResolveNeedSignatureToWinService $needSignatureToWinService
     ) {
-
     }
 
     public function __invoke(ResolveSignatureForWinCommand $command): void
     {
-        $contract = $this->contractRepository->byId($command->contractId) ?? throw ContractNotFoundException::byId($command->contractId);
-        $winner = $contract->winner() ?? throw ContractWinnerException::unresolved($contract->id());
+        $contract = $this->contractRepository->byId($command->contractId)
+            ?? throw ContractNotFoundException::byId($command->contractId);
+        $winner = $contract->winner()
+            ?? throw ContractHasNotWinnerException::unresolved($contract->id());
 
         $participants = array_map(function (Participant $participant) use ($winner): Participant {
             if (!$participant->hasUnknownSignature()) {
                 return $participant;
             }
 
-            $signaturesToWin = $this->resolveSignatureScoresService->needToWin($winner->score(), ...$participant->signatures());
-            $participant->patch(signaturesToWin: $signaturesToWin);
+            $signToWin = $this->needSignatureToWinService->needToWin($winner->score(), ...$participant->signatures());
+            $participant->patch(signaturesToWin: $signToWin);
             return $participant;
         }, $contract->participants());
 
