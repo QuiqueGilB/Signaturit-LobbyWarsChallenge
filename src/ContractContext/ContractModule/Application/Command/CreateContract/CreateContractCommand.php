49676e0da739aedb1762ae2cc6650c29332fd08b
Enrique Gil <user@enriquegil.me>
2022-10-24 19:40:03 +0200

    refactor

--- src/ContractContext/ContractModule/Application/Command/CreateContract/CreateContractCommand.php
+++ src/ContractContext/ContractModule/Application/Command/CreateContract/CreateContractCommand.php
@@ -2,18 +2,18 @@
 
 namespace Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Application\Command\CreateContract;
 
-use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Domain\Model\Participant;
+use Signaturit\LobbyWarsChallenge\ContractContext\SharedModule\Domain\Data\ParticipantData;
 use Signaturit\LobbyWarsChallenge\SharedContext\CqrsModule\Domain\Model\Command;
 use Signaturit\LobbyWarsChallenge\SharedContext\SharedModule\Domain\ValueObject\Uuid;
 
 class CreateContractCommand extends Command
 {
-    /** @var Participant[] */
+    /** @var ParticipantData[] */
     public readonly array $participants;
 
     public function __construct(
         public readonly Uuid $contractId,
-        Participant ...$participants
+        ParticipantData ...$participants
     ) {
         $this->participants = $participants;
     }
--- src/ContractContext/ContractModule/Application/Command/CreateContract/CreateContractCommandHandler.php
+++ src/ContractContext/ContractModule/Application/Command/CreateContract/CreateContractCommandHandler.php
@@ -4,7 +4,10 @@ namespace Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Applicati
 
 use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Domain\Contract\ContractRepository;
 use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Domain\Model\Contract;
+use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Domain\Model\Participant;
+use Signaturit\LobbyWarsChallenge\ContractContext\SharedModule\Domain\Data\ParticipantData;
 use Signaturit\LobbyWarsChallenge\SharedContext\CqrsModule\Domain\Contract\CommandHandler;
+use Signaturit\LobbyWarsChallenge\SharedContext\SharedModule\Domain\ValueObject\Uuid;
 
 class CreateContractCommandHandler implements CommandHandler
 {
@@ -14,7 +17,11 @@ class CreateContractCommandHandler implements CommandHandler
 
     public function __invoke(CreateContractCommand $command): void
     {
-        $contract = new Contract($command->contractId, ...$command->participants);
+        $participants = array_map(static function (ParticipantData $data): Participant {
+            return new Participant($data->participantId ?? Uuid::v4(), ...$data->signatures);
+        }, $command->participants);
+
+        $contract = new Contract($command->contractId, ...$participants);
         $this->contractRepository->save($contract);
     }
 }
