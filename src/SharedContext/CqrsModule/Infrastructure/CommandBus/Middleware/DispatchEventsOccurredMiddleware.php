c77f8cbaa56a4bd93dc25b7f6af012b33fbd0f2f
Enrique Gil <user@enriquegil.me>
2022-10-25 21:05:07 +0200

    dispatch event collector

--- src/SharedContext/CqrsModule/Infrastructure/CommandBus/Middleware/DispatchEventsOccurredMiddleware.php
+++ src/SharedContext/CqrsModule/Infrastructure/CommandBus/Middleware/DispatchEventsOccurredMiddleware.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Signaturit\LobbyWarsChallenge\SharedContext\CqrsModule\Infrastructure\CommandBus\Middleware;
 
+use Signaturit\LobbyWarsChallenge\SharedContext\CqrsModule\Domain\Contract\EventBus;
 use Signaturit\LobbyWarsChallenge\SharedContext\CqrsModule\Domain\Contract\Middleware;
 use Signaturit\LobbyWarsChallenge\SharedContext\EventSourcingModule\Domain\Service\EventCollector;
 
-class DispatchEventsMiddleware implements Middleware
+class DispatchEventsOccurredMiddleware implements Middleware
 {
-    public function __construct()
+    public function __construct(private readonly EventBus $eventBus)
     {
     }
 
@@ -18,7 +19,7 @@ class DispatchEventsMiddleware implements Middleware
         EventCollector::instance()->purge();
         $result = $next($think);
         $events = EventCollector::instance()->pull();
-
+        array_walk($events, [$this->eventBus, 'dispatch']);
 
         return $result;
     }
