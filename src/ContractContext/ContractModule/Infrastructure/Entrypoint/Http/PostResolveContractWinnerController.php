c614d38bf1fc519d648758b1d300cb917ef0779b
Enrique Gil <user@enriquegil.me>
2022-10-26 09:39:10 +0200

    refactor pass test

--- src/ContractContext/ContractModule/Infrastructure/Entrypoint/Http/PostResolveContractWinnerController.php
+++ src/ContractContext/ContractModule/Infrastructure/Entrypoint/Http/PostResolveContractWinnerController.php
@@ -7,6 +7,7 @@ namespace Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Infrastru
 use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Application\Command\CreateContract\CreateContractCommand;
 use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Application\Query\FindContract\FindContractQuery;
 use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Application\Query\FindContract\FindContractQueryResponse;
+use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Domain\Exception\ContractNotFoundException;
 use Signaturit\LobbyWarsChallenge\ContractContext\SharedModule\Domain\Data\ParticipantData;
 use Signaturit\LobbyWarsChallenge\ContractContext\SharedModule\Domain\ValueObject\Signature;
 use Signaturit\LobbyWarsChallenge\SharedContext\SharedModule\Domain\ValueObject\Uuid;
@@ -22,17 +23,43 @@ class PostResolveContractWinnerController extends Controller
     public function __invoke(Request $request): Response
     {
         $contractId = Uuid::v4();
+        $participantsData = $this->makeParticipantsData($request);
 
-        $command = new CreateContractCommand(
-            $contractId,
-            new ParticipantData(Uuid::v4(), [Signature::King]),
-            new ParticipantData(Uuid::v4(), [Signature::Notary])
-        );
+        $command = new CreateContractCommand($contractId, ...$participantsData);
         $this->commandBus->handle($command);
 
         /** @var FindContractQueryResponse $queryResponse */
         $queryResponse = $this->queryBus->ask(new FindContractQuery($contractId));
-        return new JsonResponse($queryResponse->data->winner, 201);
+        return new Response($this->makeResponse($queryResponse), 201);
     }
 
+    /** @return Signature[][] */
+    private function strToParticipantSignatures(string $signatures): array
+    {
+        return array_map(static function (string $signaturesInOneParticipant) {
+            return array_map(
+                static fn(string $signature): Signature => Signature::from($signature),
+                str_split($signaturesInOneParticipant)
+            );
+        }, explode(' ', $signatures));
+    }
+
+    /** @return ParticipantData[] */
+    private function makeParticipantsData(Request $request): array
+    {
+        $participantSignatures = $this->strToParticipantSignatures($request->getContent());
+        return array_map(static function ($signatures): ParticipantData {
+            return new ParticipantData(Uuid::v4(), $signatures);
+        }, $participantSignatures);
+    }
+
+    private function makeResponse(FindContractQueryResponse $queryResponse): string
+    {
+        return implode(
+            '',
+            array_map(
+                static fn(Signature $signature) => $signature->value,
+                $queryResponse->data->winner->signatures
+            ));
+    }
 }
