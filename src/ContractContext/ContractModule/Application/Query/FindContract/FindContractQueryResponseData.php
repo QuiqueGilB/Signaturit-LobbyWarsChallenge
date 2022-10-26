8b2ca44048f2ba5c0d85ea6409087b5820910cba
Enrique Gil <user@enriquegil.me>
2022-10-26 09:38:53 +0200

    fix bug

--- src/ContractContext/ContractModule/Application/Query/FindContract/FindContractQueryResponseData.php
+++ src/ContractContext/ContractModule/Application/Query/FindContract/FindContractQueryResponseData.php
@@ -5,16 +5,18 @@ declare(strict_types=1);
 namespace Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Application\Query\FindContract;
 
 use Signaturit\LobbyWarsChallenge\ContractContext\ContractModule\Domain\Model\Contract;
+use Signaturit\LobbyWarsChallenge\ContractContext\SharedModule\Domain\Data\ParticipantData;
 use Signaturit\LobbyWarsChallenge\SharedContext\CqrsModule\Domain\Model\QueryData;
 
 class FindContractQueryResponseData implements QueryData
 {
     public readonly string $contractId;
-    public readonly ?string $winner;
+    public readonly ?ParticipantData $winner;
 
     public function __construct(Contract $contract)
     {
         $this->contractId = $contract->id()->value;
-        $this->winner = $contract->winner();
+        $winner = $contract->winner();
+        $this->winner = $winner ? new ParticipantData($winner->id(), $winner->signatures()) : null;
     }
 }
