088eba69a4ba360b22acadf4947cd240b78f7d40
Enrique Gil <user@enriquegil.me>
2022-10-26 12:02:07 +0200

    tdd PostResolveNeedToWinControllerTest.php

---
+++ tests/Acceptance/ContractContext/ContractModule/Infrastructure/Entrypoint/Http/PostResolveNeedToWinControllerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Signaturit\LobbyWarsChallenge\Tests\Acceptance\ContractContext\ContractModule\Infrastructure\Entrypoint\Http;
+
+use Signaturit\LobbyWarsChallenge\Tests\Acceptance\SfAcceptanceHttpTest;
+use Symfony\Component\HttpFoundation\Request;
+
+class PostResolveNeedToWinControllerTest extends SfAcceptanceHttpTest
+{
+    /** @dataProvider stage */
+    public function testExistEndpoint(Request $request): void
+    {
+        $response = $this->submitRequest($request);
+        self::assertEndpointExists($response);
+    }
+
+    /** @dataProvider stage */
+    public function testStatusCode(Request $request): void
+    {
+        $response = $this->submitRequest($request);
+        self::assertEquals(201, $response->getStatusCode());
+    }
+
+    /** @dataProvider stage */
+    public function testSchemaOfResponse(Request $request): void
+    {
+        $signatures = explode(' ', $request->getContent());
+        $response = $this->submitRequest($request);
+        dump($response->getContent(), 'wiiii');
+        foreach ($signatures as $signature) {
+            self::assertStringContainsString($signature . ': ', $response->getContent());
+        }
+    }
+
+    public function stage(): iterable
+    {
+        $endpoint = '/contract/need-to-win';
+        $method = 'POST';
+
+        yield 'good request' => [Request::create(uri: $endpoint, method: $method, content: 'N V#')];
+
+    }
+}
